cc88d78eacb19a6c3f4bae2b4e965d52b2303506
Zachary Stevens <zts@cryptocracy.com>
2013-07-27 21:32:44 +0100

    berkshelf cookbook skeleton

---
+++ README.md
@@ -0,0 +1,13 @@
+# serverfault-522671 cookbook
+
+# Requirements
+
+# Usage
+
+# Attributes
+
+# Recipes
+
+# Author
+
+Author:: Zachary Stevens (<zts@cryptocracy.com>)
---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'serverfault-522671'
+maintainer       'Zachary Stevens'
+maintainer_email 'zts@cryptocracy.com'
+license          'MIT'
+description      'Installs/Configures serverfault-522671'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
